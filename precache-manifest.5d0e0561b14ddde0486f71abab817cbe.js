f3db44f381892e063234ad1f413701e924419c49
ernesthenry <henry38ernest@gmail.com>
2020-06-13 20:35:37 +0300

    Updates

---
+++ precache-manifest.5d0e0561b14ddde0486f71abab817cbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f8c92466c99900994dd097508eb8d0",
+    "url": "/reactinterface/index.html"
+  },
+  {
+    "revision": "6a265680c259b7c7340b",
+    "url": "/reactinterface/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "b4cbfe75f5af481d48a8",
+    "url": "/reactinterface/static/css/main.4d8d02ec.chunk.css"
+  },
+  {
+    "revision": "6a265680c259b7c7340b",
+    "url": "/reactinterface/static/js/2.11889860.chunk.js"
+  },
+  {
+    "revision": "e386ada3ad03a9606684b1f649731b40",
+    "url": "/reactinterface/static/js/2.11889860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4cbfe75f5af481d48a8",
+    "url": "/reactinterface/static/js/main.63c878d2.chunk.js"
+  },
+  {
+    "revision": "5f9d3750b26fd888f7bb",
+    "url": "/reactinterface/static/js/runtime-main.96b59b8e.js"
+  }
+]);
